989333f78eaccc92fc5e0a14b356729d9ab95ffe
Callum Ryan <callum.ryan@gmail.com>
2016-07-16 22:12:19 +0200

    initial commit

---
+++ ups.py
@@ -0,0 +1,28 @@
+from bs4 import BeautifulSoup
+from tabulate import tabulate
+from fake_useragent import UserAgent
+from urllib2 import Request, urlopen
+import re,urllib2
+USER_AGENT = UserAgent()
+TrackingNumbers = []
+
+with open("ups.txt", 'r') as f_in:
+	TrackingNumbers = filter(None, (line.rstrip() for line in f_in))
+
+for trcknum in TrackingNumbers:
+	print trcknum
+	track_table = []
+	req = Request("https://wwwapps.ups.com/WebTracking/track?track=yes&trackNums="+trcknum+"&loc=en_gb")
+	req.add_header('User-Agent',USER_AGENT.random)
+	page = urlopen(req).read()
+	
+	soup = BeautifulSoup(page,'html.parser')
+	table = soup.find('table',{'class':'dataTable'})
+	rows = table.findAll('tr')
+	for row in rows:
+		cols = row.findAll('td')
+		cols = [re.sub('\s+', ' ', ele.text.strip()).encode('ascii') for ele in cols]
+		if cols: track_table.append([ele for ele in cols])
+
+	print tabulate(track_table, headers =['Location','Date','Time','Info'],tablefmt="grid")
+
