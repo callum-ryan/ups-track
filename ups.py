2c4653f3653cc2786230ebfa195621c0aa365873
Callum Ryan <callum.ryan@gmail.com>
2016-07-17 19:47:43 +0200

    removed some lines to make file shorter

--- ups.py
+++ ups.py
@@ -14,14 +14,11 @@ for trcknum in TrackingNumbers:
 	track_table = []
 	req = Request("https://wwwapps.ups.com/WebTracking/track?track=yes&trackNums="+trcknum+"&loc=en_gb")
 	req.add_header('User-Agent',USER_AGENT.random)
-	page = urlopen(req).read()
-	
+	page = urlopen(req).read()	
 	soup = BeautifulSoup(page,'html.parser')
-	table = soup.find('table',{'class':'dataTable'})
-	rows = table.findAll('tr')
+	rows = soup.find('table',{'class':'dataTable'}).findAll('tr')
 	for row in rows:
-		cols = row.findAll('td')
-		cols = [re.sub('\s+', ' ', ele.text.strip()).encode('ascii') for ele in cols]
+		cols = [re.sub('\s+', ' ', ele.text.strip()).encode('ascii') for ele in row.findAll('td')]
 		if cols: track_table.append([ele for ele in cols])
 
 	print tabulate(track_table, headers =['Location','Date','Time','Info'],tablefmt="grid")
